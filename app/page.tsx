3d3cf540a65428635c72faf62ad75dea861d52dc
WebApp07 <aminezouguig@proton.me>
2025-01-28 04:38:09 -0800

    ShadCN Ui Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Button>click me</Button>
+    </div>
+  );
 };
 
 export default Home;
