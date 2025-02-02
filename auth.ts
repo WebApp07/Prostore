7f5622c8a3d3cb0c8200dc6efb9a9ab929880aed
WebApp07 <aminezouguig@proton.me>
2025-02-01 16:18:49 -0800

    Session Cart ID Cookie

--- auth.ts
+++ auth.ts
@@ -78,6 +78,30 @@ export const config = {
       }
       return token;
     },
+    authorized({ request, auth }: any) {
+      // Check for session cart cookie
+      if (!request.cookies.get("sessionCartId")) {
+        // Generate new sesssion cart id cookie
+        const sessionCartId = crypto.randomUUID();
+        //console.log(sessionCartId);
+        //return true;
+
+        // Clone the req headers.
+        const newRequestHeaders = new Headers(request.headers);
+        // Create new response and add the new headers.
+        const response = NextResponse.next({
+          request: {
+            headers: newRequestHeaders,
+          },
+        });
+
+        // Set newly generated sessionCartid in the response cookies
+        response.cookies.set("sessionCartId", sessionCartId);
+        return response;
+      } else {
+        return true;
+      }
+    },
   },
 } satisfies NextAuthConfig;
 
