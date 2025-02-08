23f79d0ab34a86f1a1ea25b815fd54c82b876b8a
WebApp07 <aminezouguig@proton.me>
2025-02-08 15:41:16 -0800

    Subtotal Card Cart Page

--- app/(root)/cart/cart-table.tsx
+++ app/(root)/cart/cart-table.tsx
@@ -16,7 +16,8 @@ import { useTransition } from "react";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { addItemToCart, removeItemFromCart } from "@/lib/actions/cart.actions";
-import { Loader, Minus, Plus } from "lucide-react";
+import { ArrowRight, Loader, Minus, Plus } from "lucide-react";
+import { Card, CardContent } from "@/components/ui/card";
 
 const CartTable = ({ cart }: { cart?: Cart }) => {
   const router = useRouter();
@@ -114,6 +115,29 @@ const CartTable = ({ cart }: { cart?: Cart }) => {
               </TableBody>
             </Table>
           </div>
+
+          <Card>
+            <CardContent className="p-4 gap-4">
+              <div className="pb-3 text-xl">
+                Subtotal ({cart.items.reduce((a, c) => a + c.qty, 0)})
+                <span className="font-bold">${cart.itemsPrice}</span>
+              </div>
+              <Button
+                className="w-full"
+                disabled={isPending}
+                onClick={() =>
+                  startTransition(() => router.push("/shipping-address"))
+                }
+              >
+                {isPending ? (
+                  <Loader className="w-4 h-4" animate-spin />
+                ) : (
+                  <ArrowRight className="w-4 h-4" />
+                )}
+                Proccesd to Checkout
+              </Button>
+            </CardContent>
+          </Card>
         </div>
       )}
     </>
