acfe1ae5adbbb9076f3799575a57d3804a73e9cb
WebApp07 <aminezouguig@proton.me>
2025-03-05 11:03:20 +0000

    Payment method page

--- app/(root)/payment-method/page.tsx
+++ app/(root)/payment-method/page.tsx
@@ -1,5 +1,26 @@
-const PaymentMethodPage = () => {
-  return <>Payment Method</>;
+import { auth } from "@/auth";
+import { getUserById } from "@/lib/actions/user.actions";
+import { Metadata } from "next";
+import PaymentMethodForm from "./payment-method-form";
+
+export const metadata: Metadata = {
+  title: "Select Payment Method",
+};
+
+const PaymentMethodPage = async () => {
+  const session = await auth();
+
+  const userId = session?.user?.id;
+
+  if (!userId) throw new Error("User not found.");
+
+  const user = await getUserById(userId);
+
+  return (
+    <>
+      <PaymentMethodForm preferredPaymentMethod={user.paymentMethod} />
+    </>
+  );
 };
 
 export default PaymentMethodPage;
