15053cb318f10163a6c8a172851c23832169d012
WebApp07 <aminezouguig@proton.me>
2025-03-06 15:37:38 +0000

    Order Zod Schemas & Types

--- lib/validators.ts
+++ lib/validators.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 import { formatNumberWithDecimal } from "./utils";
-import { PAYMENT_METHOODS } from "./constants";
+import { PAYMENT_METHOODS, shippingAddress } from "./constants";
 
 const currency = z
   .string()
@@ -85,3 +85,27 @@ export const paymentMethodSchema = z
     path: ["type"],
     message: "Ïnvalid payment method",
   });
+
+// Shcema for inserting order
+
+export const insertOrderSchema = z.object({
+  userId: z.string().min(1, "User is required"),
+  itemsPrice: currency,
+  shippingPrice: currency,
+  taxPrice: currency,
+  totalPrice: currency,
+  paymentMethod: z.string().refine((data) => PAYMENT_METHOODS.includes(data), {
+    message: "Invalid payment method",
+  }),
+  shippingAddress: shippingAddressSchema,
+});
+
+// Schema for inserting an order item
+export const insertOrderItemSchema = z.object({
+  productId: z.string(),
+  slug: z.string(),
+  image: z.string(),
+  name: z.string(),
+  price: currency,
+  qty: z.number(),
+});
