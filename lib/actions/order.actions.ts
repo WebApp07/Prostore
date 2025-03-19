e1e09c44dcf12e909141abe317d7a7168151d38d
WebApp07 <aminezouguig@proton.me>
2025-03-19 21:06:22 +0000

    fixed problem orders History

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -265,7 +265,38 @@ async function updateOrderToPaid({
   if (!updatedOrder) throw new Error("Order not found");
 }
 
-// Get user's order
+// Get user's orders
+export async function getMyOrders({
+  limit = PAGE_SIZE,
+  page,
+}: {
+  limit?: number;
+  page: number;
+}) {
+  const session = await auth();
+  if (!session) throw new Error("User is not authorized");
+
+  const data = await prisma.order.findMany({
+    where: { userId: session?.user?.id },
+    orderBy: { createdAt: "desc" },
+    take: limit,
+    skip: (page - 1) * limit,
+  });
+
+  const dataCount = await prisma.order.count({
+    where: { userId: session?.user?.id },
+  });
+
+  return {
+    data,
+    totalPages: Math.ceil(dataCount / limit),
+  };
+}
+
+type SalesDataType = {
+  month: string;
+  totalSales: number;
+}[];
 
 export async function getOrderSummary() {
   // Get counts for each resource
