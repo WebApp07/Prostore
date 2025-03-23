534d33bae2f40cac2c802ade815304e377b38d01
WebApp07 <aminezouguig@proton.me>
2025-03-23 22:16:58 +0000

    Attempted import error: 'shippingAddressDefaultValues' is not exported

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,17 +1,15 @@
-export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "ProStore";
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Prostore";
 export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
   "A modern ecommerce store built with Next.js";
-
 export const SERVER_URL =
   process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
-
 export const LATEST_PRODUCTS_LIMIT =
-  Number(process.env.LATEST_PRODUCTS_LIMIT) || 6;
+  Number(process.env.LATEST_PRODUCTS_LIMIT) || 4;
 
 export const signInDefaultValues = {
-  email: "",
-  password: "",
+  email: "admin@example.com",
+  password: "123456",
 };
 
 export const signUpDefaultValues = {
@@ -21,18 +19,17 @@ export const signUpDefaultValues = {
   confirmPassword: "",
 };
 
-export const shippingAddress = {
-  fullNmae: "John Doe",
-  streetAddress: "123 Main st",
-  city: "Anytown",
-  postalCode: "12345",
-  country: "USA",
+export const shippingAddressDefaultValues = {
+  fullName: "",
+  streetAddress: "",
+  city: "",
+  postalCode: "",
+  country: "",
 };
 
-export const PAYMENT_METHOODS = process.env.PAYMENT_METHODS
+export const PAYMENT_METHODS = process.env.PAYMENT_METHODS
   ? process.env.PAYMENT_METHODS.split(", ")
-  : ["PayPal"];
-
+  : ["PayPal", "Stripe", "CashOnDelivery"];
 export const DEFAULT_PAYMENT_METHOD =
   process.env.DEFAULT_PAYMENT_METHOD || "PayPal";
 
@@ -56,3 +53,9 @@ export const productDefaultValues = {
 export const USER_ROLES = process.env.USER_ROLES
   ? process.env.USER_ROLES.split(", ")
   : ["admin", "user"];
+
+export const reviewFormDefaultValues = {
+  title: "",
+  comment: "",
+  rating: 0,
+};
