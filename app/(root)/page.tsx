75603a7c7387a68f650ac17c135962203b6656ed
WebApp07 <aminezouguig@proton.me>
2025-03-23 02:27:24 +0000

    Featured Products Carousel

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,21 @@
+import ProductCarousel from "@/components/shared/product/product-carousel";
 import ProductList from "@/components/shared/product/product-list";
-import { getLatestProducts } from "@/lib/actions/product.actions";
+import {
+  getLatestProducts,
+  getFeaturedProdcuts,
+} from "@/lib/actions/product.actions";
 
 const Home = async () => {
   const latestProducts = await getLatestProducts();
+  const featuredProducts = await getFeaturedProdcuts();
 
   return (
-    <div>
+    <>
+      {featuredProducts.length > 0 && (
+        <ProductCarousel data={featuredProducts} />
+      )}
       <ProductList data={latestProducts} title="Newest Arrivals" />
-    </div>
+    </>
   );
 };
 
--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -131,3 +131,14 @@ export async function getAllCategories() {
 
   return data;
 }
+
+// Get featured products
+export async function getFeaturedProdcuts() {
+  const data = await prisma.product.findMany({
+    where: { isFeatured: true },
+    orderBy: { createdAt: "desc" },
+    take: 4,
+  });
+
+  return convertToPlainObject(data);
+}
