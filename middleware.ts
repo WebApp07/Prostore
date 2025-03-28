649faf5c435d42ebaafebc60fa2cf6a89c829e1e
WebApp07 <aminezouguig@proton.me>
2025-03-28 03:13:47 +0000

    Middleware 1mb

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,9 @@
+<<<<<<< HEAD
 import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
+=======
+import NextAuth from "next-auth";
+import { authConfig } from "./auth.config";
+>>>>>>> 6e92d63 (Middleware 1mb)
 
 export const { auth: middleware } = NextAuth(authConfig);
